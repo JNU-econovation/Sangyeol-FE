da06021b6d96a06cbd49c7d41d0dd5611e87b20a
박건규 <geongyu09@gmail.com>
2025-08-26 21:47:24 +0900

    feat: HomeScreen을 변경된 ui에 맞추어 배경롸 레이아웃 추가 및 수정
    #2

--- packages/app/src/app/(tabs)/home/index.tsx
+++ packages/app/src/app/(tabs)/home/index.tsx
@@ -1,26 +1,22 @@
 import styled from "@emotion/native";
-import HomeCanvas from "@screens/Home/HomeCanvas";
-import HomeHeaderSection from "@screens/Home/HomeHeaderSection";
+import { ImageBackground } from "react-native";
 import HomeNavGridSection from "@screens/Home/HomeNavGridSection";
-import ScreenContainer from "@shared/layout/Screen";
+// import ScreenContainer from "@shared/layout/Screen";
 import Spacing from "@shared/layout/Spacing";
 
 const HomeScreen = () => {
   return (
-    <ScreenContainer backgroundColor="subGray">
-      <Container>
-        <Spacing size={20} />
-        <HomeHeaderSection />
-        <Spacing size={20} />
-        <HomeCanvas />
-        <HomeNavGridSection />
-        <Spacing size={20} />
-      </Container>
-    </ScreenContainer>
+    <Container source={require("@assets/images/Home_Background.png")}>
+      <HomeNavGridSection />
+      <Spacing size={20} />
+    </Container>
   );
 };
-const Container = styled.View`
+const Container = styled(ImageBackground)`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
   padding-inline: 20px;
 `;
 
