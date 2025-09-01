7991a982609c214be7bd7165f2af7df40db58de3
박건규 <geongyu09@gmail.com>
2025-09-01 17:13:30 +0900

    feat(web): 공통으로 사용되는 Course 타입 정의
    #4

--- packages/web/src/types/course/index.ts
+++ packages/web/src/types/course/index.ts
@@ -1,2 +1,12 @@
 // export type CourseDifficulty = "쉬움" | "보통" | "어려움";
 export type CourseDifficulty = "EASY" | "NORMAL" | "HARD";
+
+export interface Course {
+  id: string;
+  name: string;
+  length: number;
+  duration: number;
+  difficulty: CourseDifficulty;
+  bookmark: boolean;
+  image: string;
+}
