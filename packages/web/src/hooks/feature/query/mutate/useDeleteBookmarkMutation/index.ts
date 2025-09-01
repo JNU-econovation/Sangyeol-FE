11cfa7d085719b4580078b4e142923aa3b4724a7
박건규 <geongyu09@gmail.com>
2025-09-01 17:15:59 +0900

    feat(web): useDeleteBookmarkMutation 에 낙관적 업데이트 추가
    #4

--- packages/web/src/hooks/feature/query/mutate/useDeleteBookmarkMutation/index.ts
+++ packages/web/src/hooks/feature/query/mutate/useDeleteBookmarkMutation/index.ts
@@ -3,17 +3,77 @@ import {
   DELETE_BOOKMARK_API_PATH,
   deleteBookmarkApi,
 } from "@/api/v1/bookmarks/[courseId]";
+import {
+  COURSES_OF_MOUNTAIN_API_PATH,
+  CourseSortType,
+  type GetCoursesOfMountainResponse,
+} from "@/api/v1/mountains/[mountainId]/courses";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const useDeleteBookmarkMutation = () => {
+interface UseDeleteBookmarkMutationProps {
+  mountainId: string;
+  sortBy: CourseSortType;
+}
+
+const useDeleteBookmarkMutation = ({
+  mountainId,
+  sortBy,
+}: UseDeleteBookmarkMutationProps) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: [DELETE_BOOKMARK_API_PATH],
     mutationFn: deleteBookmarkApi,
+
+    onMutate: (selectedCourseId) => {
+      // const prev = queryClient.getQueryData([BOOKMARK_API_PATH]);
+
+      // 코스 리스트 데이터가 들어있다.
+      const prevCourseListData =
+        queryClient.getQueryData<GetCoursesOfMountainResponse>([
+          COURSES_OF_MOUNTAIN_API_PATH(mountainId, {
+            searchParams: { sortBy },
+          }),
+        ]).courses;
+      const newCourseListData = prevCourseListData.map((course) => {
+        if (course.id === selectedCourseId) {
+          return {
+            ...course,
+            bookmark: false,
+          };
+        }
+        return course;
+      });
+
+      queryClient.setQueryData(
+        [
+          COURSES_OF_MOUNTAIN_API_PATH(mountainId, {
+            searchParams: { sortBy },
+          }),
+        ],
+        {
+          courses: newCourseListData,
+        },
+      );
+
+      return prevCourseListData;
+    },
+
     onSuccess: () => {
       // TODO: 낙관적 업데이트로 변경하기
       queryClient.invalidateQueries({ queryKey: [BOOKMARK_API_PATH] });
+    },
+    onError: (_, __, context) => {
+      queryClient.setQueryData(
+        [
+          COURSES_OF_MOUNTAIN_API_PATH(mountainId, {
+            searchParams: { sortBy },
+          }),
+        ],
+        {
+          courses: context,
+        },
+      );
     },
   });
 };
