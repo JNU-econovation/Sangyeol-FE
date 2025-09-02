d011b1b3cd28ca7bfdef3fafc464635e921b36f2
박건규 <geongyu09@gmail.com>
2025-09-02 12:29:22 +0900

    fix(web): useBasesQuery 애서 mountainId값을 불필요하게 에러를 피하기 위하여 nullable하지 않음에도 ??연산자를 사용하는 부분 제거
    #

--- packages/web/src/hooks/feature/query/query/useBasesQuery/index.ts
+++ packages/web/src/hooks/feature/query/query/useBasesQuery/index.ts
@@ -7,8 +7,8 @@ interface UseBaseQueryProps {
 
 const useBasesQuery = ({ mountainId }: UseBaseQueryProps) => {
   return useSuspenseQuery({
-    queryKey: [BASES_API_PATH(mountainId ?? "1")],
-    queryFn: () => getBasesApi(mountainId ?? "1"),
+    queryKey: [BASES_API_PATH(mountainId)],
+    queryFn: () => getBasesApi(mountainId),
     staleTime: 1000 * 60 * 5, // 5m
     gcTime: 1000 * 60 * 60, // 1h
   });
