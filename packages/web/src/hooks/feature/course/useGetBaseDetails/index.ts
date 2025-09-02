275109a42fddb825ef939f6d367cc9b4a4143dad
박건규 <geongyu09@gmail.com>
2025-09-02 12:30:39 +0900

    feat(web): useGetBaseDetails 훅 구현
    useBasesQuery, useBasesDetailQuery 를 통하여 얻은 거점 정보들을 바탕으로, 인자로 받은 baseId에 해당하는 거점 데이터를 반환하는 훅 구현
    
    #8

---
+++ packages/web/src/hooks/feature/course/useGetBaseDetails/index.ts
@@ -0,0 +1,31 @@
+import useBasesDetailQuery from "@hooks/feature/query/query/useBasesDetailQuery";
+import useBasesQuery from "@hooks/feature/query/query/useBasesQuery";
+
+interface UseGetBaseDetailsProps {
+  mountainId: string;
+  baseId: string;
+}
+
+const useGetBaseDetails = ({ mountainId, baseId }: UseGetBaseDetailsProps) => {
+  const {
+    data: { baseDetails },
+  } = useBasesDetailQuery({
+    mountainId,
+  });
+
+  const {
+    data: { bases },
+  } = useBasesQuery({ mountainId });
+
+  const indexOfBases = bases.findIndex((base) => +base.baseId === +baseId);
+  const indexOfBaseDetails = baseDetails.findIndex(
+    (detail) => +detail.baseId === +baseId,
+  );
+
+  if (indexOfBases === -1 || indexOfBaseDetails === -1)
+    throw new Error("[useGetBaseDetails] 존재하지 않는 베이스입니다.");
+
+  return { ...baseDetails?.[indexOfBaseDetails], ...bases[indexOfBases] };
+};
+
+export default useGetBaseDetails;
