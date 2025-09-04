7ef396ae8d4678269f13e3a3ce493053ebd71300
박건규 <geongyu09@gmail.com>
2025-09-04 15:26:31 +0900

    feat(web): SelectorCloseIcon 구현
    #8

---
+++ packages/web/src/components/common/shared/ui/icons/SelectorCloseIcon/index.tsx
@@ -0,0 +1,22 @@
+import Selector_Close from "@/icons/Selector_Close.svg";
+import Image from "next/image";
+import { ComponentProps } from "react";
+
+const DEFAULT_ALT = "선택기 닫기";
+
+interface SelectorCloseIconProps
+  extends Omit<
+    ComponentProps<typeof Image>,
+    "src" | "width" | "height" | "alt"
+  > {
+  alt?: string;
+  width?: number;
+  height?: number;
+}
+
+export default function SelectorCloseIcon({
+  alt = DEFAULT_ALT,
+  ...props
+}: SelectorCloseIconProps) {
+  return <Image src={Selector_Close} alt={alt} {...props} />;
+}
