1fdd293d601b93c0f3aad20f1d9f0c0a070dc217
박건규 <geongyu09@gmail.com>
2025-09-14 17:33:02 +0900

    feat: start-travel 스팩이 변경됨에 따라서 기능 수정
    #10

--- packages/web/src/hooks/feature/bridge/useStartTravelBridge/index.ts
+++ packages/web/src/hooks/feature/bridge/useStartTravelBridge/index.ts
@@ -5,19 +5,22 @@ import type {
 } from "@/types/bridge";
 import { useCallback } from "react";
 
+/**
+ * 코스를 선택한 산행 시작 브릿지 훅
+ */
 const useStartTravelBridge = () => {
   const { request } = useBridge<
-    MessageEventRequestData<{ courseId: string }>,
+    MessageEventRequestData<{ courseId: string; mountainId: string }>,
     MessageEventResponseData
   >();
 
   return useCallback(
-    (courseId: string) => {
+    ({ courseId, mountainId }: { courseId: string; mountainId: string }) => {
       request({
         requestMessage: {
           name: "start-travel",
           method: "POST",
-          body: { courseId },
+          body: { courseId, mountainId },
         },
       });
     },
