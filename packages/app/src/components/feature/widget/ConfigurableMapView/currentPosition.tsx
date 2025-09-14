a57ed3e5ca2006f213e417f9b4d623293f09780f
박건규 <geongyu09@gmail.com>
2025-09-14 17:40:58 +0900

    feat(app): 서비스에서 필요한 모든 기능을 가진 공통 지도 컴포넌트 구현
    여기서 너무 로직이 복잡해지는 것을 막기 위하여 부가적인 컴포넌트로 책임분리를 하였습니다.
    다만, 완벽하지  않으며, 추후에 리팩토링이 필요합니다.
    
    #10

---
+++ packages/app/src/components/feature/widget/ConfigurableMapView/currentPosition.tsx
@@ -0,0 +1,24 @@
+import useRealTimeLocation from "@hooks/feature/useRealTimeLocation";
+import { NaverMapMarkerOverlay } from "@mj-studio/react-native-naver-map";
+import { memo } from "react";
+
+const CurrentPosition = memo(() => {
+  const { location, isLoading: isLocationLoading } = useRealTimeLocation({
+    accuracy: "highest",
+    timeInterval: 2000,
+    distanceInterval: 2,
+  });
+
+  if (isLocationLoading || !location) return null;
+
+  return (
+    <NaverMapMarkerOverlay
+      latitude={location.coords.latitude}
+      longitude={location.coords.longitude}
+      image={require("@assets/images/Current_Position.png")}
+      anchor={{ x: 0.5, y: 0.5 }}
+    />
+  );
+});
+
+export default CurrentPosition;
---
+++ packages/app/src/components/feature/widget/ConfigurableMapView/index.tsx
@@ -0,0 +1,130 @@
+import MOUNTAIN from "@constants/mountain";
+import useRealTimeLocation from "@hooks/feature/useRealTimeLocation";
+import {
+  NaverMapMarkerOverlay,
+  NaverMapPathOverlay,
+  NaverMapPathOverlayProps,
+  NaverMapView,
+} from "@mj-studio/react-native-naver-map";
+import { Coordinate } from "@model/map";
+import { memo, useState } from "react";
+
+import CurrentPosition from "./currentPosition";
+import DomainMarkers from "./domainMarkers";
+import HeadPolygon from "./headPolygon";
+import type { NaverMapMarkerProps } from "./marker";
+// import NaverMapMarker from "./marker";
+
+const DEFAULT_ZOOM = 14;
+
+export interface PolylineOptions extends NaverMapPathOverlayProps {}
+
+export interface ConfigurableMapViewProps {
+  paths?: PolylineOptions[];
+  marker?: { latitude: number; longitude: number };
+  currentPositionIcon?: boolean;
+  zoom?: number;
+
+  markers?: NaverMapMarkerProps[];
+  showMarkers?: boolean;
+
+  //TODO: 너무 도메인에 강결합된 값. 하지만.. 이거 어떻게 해결해.. NaverMapMarker로 하면 메모리 초과로 앱 죽음
+  showOverlays: string[];
+  bases?: Coordinate[];
+  toilets?: Coordinate[];
+  markets?: Coordinate[];
+  rentals?: Coordinate[];
+  emergencyKits?: Coordinate[];
+}
+
+const ConfigurableMapView = memo(
+  ({
+    zoom = DEFAULT_ZOOM,
+    paths,
+    // showMarkers,
+    // markers,
+    showOverlays = [],
+    bases,
+    toilets,
+    markets,
+    rentals,
+    emergencyKits,
+  }: ConfigurableMapViewProps) => {
+    const { location, isLoading: isLocationLoading } = useRealTimeLocation({
+      accuracy: "highest",
+      timeInterval: 2000,
+      distanceInterval: 2,
+    });
+
+    const [zoomLevel, setZoomLevel] = useState(zoom);
+
+    if (isLocationLoading || !location) {
+      return null;
+    }
+
+    return (
+      <NaverMapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        }}
+        isShowLocationButton={false}
+        initialCamera={{
+          zoom: zoomLevel,
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        }}
+        isShowZoomControls={false}
+        isShowCompass={false}
+        onCameraChanged={({ zoom }) => {
+          setZoomLevel(zoom);
+        }}
+        minZoom={6}
+      >
+        <CurrentPosition />
+        <HeadPolygon zoomLevel={zoomLevel} />
+
+        <DomainMarkers
+          zoomLevel={zoomLevel}
+          showOverlays={showOverlays}
+          bases={bases}
+          toilets={toilets}
+          markets={markets}
+          rentals={rentals}
+          emergencyKits={emergencyKits}
+        />
+
+        {/* markers */}
+        {Object.keys(MOUNTAIN).map((key) => {
+          const [longitude, latitude] =
+            MOUNTAIN[key as keyof typeof MOUNTAIN].coordinate;
+          return (
+            <NaverMapMarkerOverlay
+              key={key}
+              latitude={latitude}
+              longitude={longitude}
+              image={require("@assets/images/Mountain.png")}
+              isHidden={zoomLevel >= 9 && showOverlays.length !== 0}
+            />
+          );
+        })}
+
+        {paths &&
+          paths.map(
+            (pathOptions, index) =>
+              pathOptions.coords.length > 3 && (
+                <NaverMapPathOverlay
+                  key={`path-${index}`}
+                  coords={pathOptions.coords}
+                />
+              ),
+          )}
+      </NaverMapView>
+    );
+  },
+);
+
+export default ConfigurableMapView;
