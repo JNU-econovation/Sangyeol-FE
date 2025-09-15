4dfb45feb81befc8ae570a5d8f9b5d2dd3885744
박건규 <geongyu09@gmail.com>
2025-09-15 21:46:12 +0900

    chore(web): next.config.ts 에 transpilePackages 추가
    #10

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   devIndicators: false,
+  transpilePackages: ["bridge", "stack-link"],
   images: {
     remotePatterns: process.env.IMAGE_REMOTE_URL
       ? [new URL(process.env.IMAGE_REMOTE_URL)]
