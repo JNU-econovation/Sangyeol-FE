e345d379b41863eb39593912ce14fcb74303979d
박건규 <geongyu09@gmail.com>
2025-09-16 22:31:18 +0900

    feat(app): TravelNavIcon 구현
    #10

--- packages/app/src/app/travel/withoutCourse/withoutCourseTravel.tsx
+++ packages/app/src/app/travel/withoutCourse/withoutCourseTravel.tsx
@@ -1,19 +1,27 @@
 // import TravelWithoutCourseMapWebview from "@screens/travel/TravelWithoutCourseMapWebview";
+import styled from "@emotion/native";
 import TravelMonitorSection from "@screens/travel/TravelMonitorSection";
+import TravelNavIcon from "@screens/travel/TravelNavIcon";
 import TravelWithoutCourseMap from "@screens/travel/TravelWithoutCourseMap";
 import PositionBottom from "@shared/layout/PositionBottom";
 import Spacing from "@shared/layout/Spacing";
 
 const WithoutCourseTravel = () => {
   return (
-    <>
+    <Container>
       <Spacing size={16} />
       <TravelWithoutCourseMap />
+      <TravelNavIcon />
       <PositionBottom bottom={24}>
         <TravelMonitorSection />
       </PositionBottom>
-    </>
+    </Container>
   );
 };
+
+const Container = styled.View`
+  flex: 1;
+  position: relative;
+`;
 
 export default WithoutCourseTravel;
