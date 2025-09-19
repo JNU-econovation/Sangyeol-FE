902b44b157131dc63b3baa4401f068b78a3c6646
박건규 <geongyu09@gmail.com>
2025-09-20 00:17:49 +0900

    feat(app): TravelWithCourseMonitorSection 구현
    #10

---
+++ packages/app/src/components/feature/screens/travel/TravelWithCourseMonitorSection/index.tsx
@@ -0,0 +1,167 @@
+import styled from "@emotion/native";
+import TravelContinueButton from "@screens/travel/TravelContinueButton";
+import TravelEndButton from "@screens/travel/TravelEndButton";
+import TravelPauseButton from "@screens/travel/TravelPauseButton";
+import Spacing from "@shared/layout/Spacing";
+import Text from "@shared/ui/Text";
+import useTravelStateStore from "@store/travel";
+import { COLORS } from "@styles/colorPalette";
+import { msToTimeText } from "@utils/time";
+import { useEffect, useState } from "react";
+
+const INTERVAL_CYCLE = 500; // 500 milliseconds
+
+const TravelWithCourseMonitorSection = () => {
+  const [elapsedTime, setElapsedTime] = useState(0); // milliseconds. 산행 시간
+  const {
+    distance,
+    travelState,
+    getElapsedTime,
+    remainTimeToEnd,
+    remainTimeToStopover,
+  } = useTravelStateStore();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setElapsedTime(getElapsedTime()); //TODO: 상태를 상태로 넣고 있음. 수정 필요
+    }, INTERVAL_CYCLE);
+    return () => clearInterval(interval);
+  }, [getElapsedTime, travelState]);
+
+  return (
+    <Container>
+      <Spacing size={24} />
+      <ButtonContainer>
+        {/* 여행 상태에 따른 버튼 렌더링 */}
+        {travelState === "in-progress" && (
+          <>
+            <TravelPauseButton />
+            <TravelEndButton />
+          </>
+        )}
+        {travelState === "paused" && (
+          <>
+            <TravelContinueButton />
+            <TravelEndButton />
+          </>
+        )}
+      </ButtonContainer>
+
+      <TimeDisplay>
+        {travelState === "idle" ? "--:--" : msToTimeText(elapsedTime)}
+      </TimeDisplay>
+      <Text textAlign="center" opacity={0.5}>
+        산행 시간
+      </Text>
+
+      <Spacing size={12} />
+
+      <MetricsContainer>
+        <MetricItem>
+          <MetricIcon>🏃‍♂️‍➡️</MetricIcon>
+          <MetricData>
+            <MetricValue>{distance.toFixed(2)}</MetricValue>
+            <MetricUnit>km</MetricUnit>
+          </MetricData>
+        </MetricItem>
+
+        <Divider />
+
+        <MetricItem>
+          <MetricIcon>🔥</MetricIcon>
+          <MetricData disabled={!remainTimeToStopover}>
+            <MetricValue>
+              {remainTimeToStopover === null ? "--:--" : remainTimeToStopover}
+            </MetricValue>
+            <MetricUnit>경유지까지</MetricUnit>
+          </MetricData>
+        </MetricItem>
+
+        <Divider />
+        <MetricItem>
+          <MetricIcon>⛰️</MetricIcon>
+          <MetricData>
+            <MetricValue disabled={!remainTimeToEnd}>
+              {remainTimeToEnd === null ? "--:--" : remainTimeToEnd}
+            </MetricValue>
+            <MetricUnit>도착지까지</MetricUnit>
+          </MetricData>
+        </MetricItem>
+      </MetricsContainer>
+      <Spacing size={16} />
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  position: relative;
+  background-color: ${COLORS.mainWhite};
+  border-radius: 12px;
+  z-index: 50;
+  padding: 0 16px;
+  width: 100%;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  height: 24px;
+  transform: translateY(-32px);
+`;
+
+const TimeDisplay = styled.Text`
+  font-size: 32px;
+  font-weight: 600;
+  text-align: center;
+  color: ${COLORS.black};
+`;
+
+const MetricsContainer = styled.View`
+  background-color: ${COLORS.green500};
+  border-radius: 12px;
+  padding: 16px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MetricItem = styled.View`
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+`;
+
+const MetricIcon = styled.Text`
+  font-size: 16px;
+`;
+
+const MetricData = styled.View<{ disabled?: boolean }>`
+  align-items: flex-start;
+  opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
+  color: ${({ disabled }) => (disabled ? COLORS["black-800"] : COLORS.black)};
+`;
+
+const MetricValue = styled.Text<{ disabled?: boolean }>`
+  font-weight: 600;
+  font-size: 16px;
+  color: ${({ disabled }) => (disabled ? COLORS["black-800"] : COLORS.black)};
+`;
+
+const MetricUnit = styled.Text`
+  font-size: 12px;
+  color: ${COLORS["black-800"]};
+  font-weight: 400;
+  font-size: 11px;
+  text-align: right;
+`;
+
+const Divider = styled.View`
+  height: 40px;
+  width: 1px;
+  background-color: ${COLORS.gray900};
+  opacity: 0.5;
+`;
+
+export default TravelWithCourseMonitorSection;
