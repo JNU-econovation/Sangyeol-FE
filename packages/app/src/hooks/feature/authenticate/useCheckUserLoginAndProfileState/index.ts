6303d215efae2cc4b52d423e0049baede16910cd
박건규 <geongyu09@gmail.com>
2025-09-20 23:34:35 +0900

    feat(app): 로그이 여부 및 프로핗 설정 여부를 확인하는 useCheckUserLoginAndProfileState 훅 구션
    #10

---
+++ packages/app/src/hooks/feature/authenticate/useCheckUserLoginAndProfileState/index.ts
@@ -0,0 +1,48 @@
+import useUserProfileStatusQuery from "@hooks/feature/query/query/useUserProfileStatusQuery";
+import { getValueFromSecureStore } from "@utils/secureStore";
+import { useCallback, useEffect, useState } from "react";
+
+const useCheckUserLoginAndProfileState = () => {
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  const [isCheckingLoginLoading, setIsCheckingLoginLoading] = useState(true);
+  const {
+    data: profileStatus,
+    isLoading: profileStatusLoading,
+    error: profileStatusError,
+  } = useUserProfileStatusQuery();
+
+  const checkLogin = useCallback(async () => {
+    try {
+      const existentAccessToken = await getValueFromSecureStore("accessToken");
+      const existentRefreshToken =
+        await getValueFromSecureStore("refreshToken");
+
+      if (existentAccessToken && existentRefreshToken) {
+        setAccessToken(existentAccessToken);
+        setRefreshToken(existentRefreshToken);
+      }
+    } catch (error) {
+      console.error("[global index] Error checking login status:", error);
+    } finally {
+      setIsCheckingLoginLoading(false);
+    }
+  }, []);
+
+  // 처음 렌더링 될 때, 로그인 상태 확인
+  useEffect(() => {
+    checkLogin();
+  }, [checkLogin]);
+
+  return {
+    isLoggedIn: !!accessToken,
+    isProfileComplete: profileStatus?.isComplete ?? false,
+    isLoading: profileStatusLoading || isCheckingLoginLoading,
+    error: profileStatusError,
+
+    accessToken,
+    refreshToken,
+  };
+};
+
+export default useCheckUserLoginAndProfileState;
