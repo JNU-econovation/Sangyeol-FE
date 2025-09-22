8e840e151d6304174fbfb6f0a26a8593519e4197
박건규 <geongyu09@gmail.com>
2025-09-22 22:28:49 +0900

    fix(app): ProfileForm에서 키보드에 의하여 input이 겹쳐서 보이지 않던 문제 해결
    해당 부분에 Scroll view와 KeyboardAvoidingView를 적용하여 해결하였습니다
    
    #13

--- packages/app/src/components/feature/screens/onboarding/ProfileForm/index.tsx
+++ packages/app/src/components/feature/screens/onboarding/ProfileForm/index.tsx
@@ -2,35 +2,43 @@ import styled from "@emotion/native";
 import useProfileSetForm from "@hooks/feature/form/useProfileSetForm";
 import Spacing from "@shared/layout/Spacing";
 import { FormProvider } from "react-hook-form";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 // form field
 import EmailField from "./EmailField";
 import NicknameField from "./NicknameField";
 import PhoneNumberField from "./PhoneNumberField";
-import VerificationField from "./VerificationField";
 import SubmitButton from "./SubmitButton";
+import VerificationField from "./VerificationField";
 
 const ProfileForm = () => {
   const form = useProfileSetForm();
 
   return (
-    <FormProvider {...form}>
-      <FormContainer>
-        <NicknameField />
-        <Spacing size={14} />
-        <PhoneNumberField />
-        <Spacing size={14} />
-        <VerificationField />
-        <Spacing size={14} />
-        <EmailField />
-      </FormContainer>
-      <SubmitButton />
-    </FormProvider>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+      style={{ flex: 1 }}
+    >
+      <FormProvider {...form}>
+        <FormContainer>
+          <NicknameField />
+          <Spacing size={14} />
+          <PhoneNumberField />
+          <Spacing size={14} />
+          <VerificationField />
+          <Spacing size={14} />
+          <EmailField />
+          <Spacing size={60} />
+          <SubmitButton />
+        </FormContainer>
+      </FormProvider>
+    </KeyboardAvoidingView>
   );
 };
 
-const FormContainer = styled.View`
+const FormContainer = styled.ScrollView`
   padding-inline: 24px;
+  flex: 1;
 `;
 
 export default ProfileForm;
