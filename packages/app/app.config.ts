d37195834b2fa1255dfa8f4717a287b066149132
박건규 <geongyu09@gmail.com>
2025-09-22 22:26:00 +0900

    chore(app): react-native-keyboard-controller를 안드로이드에서 사용할 수 있도록 softwareKeyboardLayoutMode를 pan으로 설정
    #13

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -24,6 +24,7 @@ const appConfig = {
       },
     },
     android: {
+      softwareKeyboardLayoutMode: "pan",
       adaptiveIcon: {
         foregroundImage: "./assets/images/splash-icon.png",
         backgroundColor: "#ffffff",
