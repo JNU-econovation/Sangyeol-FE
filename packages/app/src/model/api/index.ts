ec29a0a5e360eed1b280c3f6ff7284ae1fd29a93
박건규 <geongyu09@gmail.com>
2025-09-23 15:53:12 +0900

    feat(app): api 에러 타입을 Error 타입을 확장하도록 수정
    #13

--- packages/app/src/model/api/index.ts
+++ packages/app/src/model/api/index.ts
@@ -1,5 +1,8 @@
-export interface ErrorResponse {
+import ERROR_CODES from "@constants/api/error";
+
+// API 에러 응답 타입
+export interface ErrorResponse extends Error {
   status: "error";
-  errorCode: string;
+  errorCode: keyof typeof ERROR_CODES;
   message: string;
 }
