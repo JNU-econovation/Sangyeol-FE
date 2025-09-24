9f38e27b6203c0a0c056e6c2fae7f81c5c246a89
박건규 <geongyu09@gmail.com>
2025-09-24 18:29:50 +0900

    feat(app): Spacing 컴포넌트에서 좌우로 간격을 줄 수 있는 horizontal props 추가
    #17

--- packages/app/src/components/common/shared/layout/Spacing/index.tsx
+++ packages/app/src/components/common/shared/layout/Spacing/index.tsx
@@ -3,10 +3,15 @@ import { memo } from "react";
 
 interface SpacingProps {
   size?: number;
+  horizontal?: boolean;
 }
 
 const Spacing = memo(styled.View<SpacingProps>`
   margin-top: ${({ size }) => (size ? `${size}px` : "0")};
+  margin-left: ${({ horizontal, size }) =>
+    horizontal && size ? `${size}px` : "0"};
+  margin-right: ${({ horizontal, size }) =>
+    horizontal && size ? `${size}px` : "0"};
 `);
 
 export default Spacing;
