99561a966c675cc4cc1a69531b4bcd20e6576d1d
박건규 <geongyu09@gmail.com>
2025-09-27 23:07:27 +0900

    feat(app): 이미지 선택 폼 섹션 추가
    #17

--- packages/app/src/components/common/entities/ImageSelectField/index.tsx
+++ packages/app/src/components/common/entities/ImageSelectField/index.tsx
@@ -1,10 +1,14 @@
+import { TrashSVG } from "@components/common/shared/ui/Icons";
+import Text from "@components/common/shared/ui/Text";
 import styled from "@emotion/native";
 import useImagePicker from "@hooks/common/useImagePicker";
 import FieldLayout from "@shared/layout/FieldLayout";
 import WeakButton from "@shared/ui/WeakButton";
+import { COLORS } from "@styles/colorPalette";
 
 interface ImageSelectFieldProps {
   title: string;
+  titleButton?: boolean;
   buttonTitle: string;
   onChange?: (uris: string[]) => void;
   value?: string[];
@@ -12,28 +16,42 @@ interface ImageSelectFieldProps {
 
 const ImageSelectField = ({
   title,
+  titleButton = false,
   buttonTitle,
   onChange,
 }: ImageSelectFieldProps) => {
-  const { selectedImagesUri, pickImage } = useImagePicker({ onChange });
+  const { selectedImagesUri, pickImage, removeImage } = useImagePicker({
+    onChange,
+  });
 
   return (
     <FieldLayout
       title={title}
       titleSideComponent={
-        <WeakButton title={buttonTitle} onPress={pickImage} />
+        titleButton && <WeakButton title={buttonTitle} onPress={pickImage} />
       }
       content={
         <ImageContainer>
           {selectedImagesUri &&
             selectedImagesUri.map((uri, index) => (
-              <AttachmentItem
-                key={`${uri}-${index}`}
-                source={{ uri }}
-                style={{ width: 85, height: 85, borderRadius: 8 }}
-              />
+              <AttachmentItemContainer key={`${uri}-${index}`}>
+                <AttachmentItem source={{ uri }} />
+                <AttachmentItemDeleteButton
+                  onPress={() => {
+                    removeImage(index);
+                  }}
+                >
+                  <TrashSVG />
+                </AttachmentItemDeleteButton>
+              </AttachmentItemContainer>
             ))}
-          {selectedImagesUri?.length === 0 && <EmptyAttachmentItem />}
+          {(!selectedImagesUri || selectedImagesUri?.length < 3) && (
+            <EmptyAttachmentItem onPress={pickImage}>
+              <Text color="gray900" fontSize={40} textAlign="center">
+                +
+              </Text>
+            </EmptyAttachmentItem>
+          )}
         </ImageContainer>
       }
     />
@@ -42,22 +60,34 @@ const ImageSelectField = ({
 
 const ImageContainer = styled.View`
   flex-direction: row;
+  gap: 20px;
 `;
 
-const AttachmentItem = styled.Image`
+const AttachmentItemContainer = styled.View`
+  position: relative;
+`;
+
+const AttachmentItem = styled.Image<{ uri?: string }>`
   width: 85px;
   height: 85px;
   border-radius: 8px;
   background-color: #f0f0f0;
 `;
 
-const EmptyAttachmentItem = styled.View`
+const EmptyAttachmentItem = styled.TouchableOpacity`
   width: 85px;
   height: 85px;
   border-radius: 8px;
-  background-color: #f0f0f0;
+  background-color: ${COLORS.gray300};
   justify-content: center;
   align-items: center;
+`;
+
+const AttachmentItemDeleteButton = styled.TouchableOpacity`
+  position: absolute;
+  top: 2px;
+  right: 2px;
+  padding: 8px;
 `;
 
 export default ImageSelectField;
