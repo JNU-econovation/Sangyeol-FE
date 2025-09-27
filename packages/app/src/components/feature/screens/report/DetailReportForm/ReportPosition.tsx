934f462c09526e7b8281b7837207ca8f6e6b7886
박건규 <geongyu09@gmail.com>
2025-09-27 23:14:25 +0900

    feat(app): 현재 위치 폴 섹션 추가
    #17

---
+++ packages/app/src/components/feature/screens/report/DetailReportForm/ReportPosition.tsx
@@ -0,0 +1,9 @@
+import PositionSelectField from "@entities/PositionSelectField";
+
+const ReportPosition = () => {
+  return (
+    <PositionSelectField title="현재 위치" titleSideButtonTitle="현재 위치 +" />
+  );
+};
+
+export default ReportPosition;
