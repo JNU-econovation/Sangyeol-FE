0d698e38ac73b78bce28dad8974a54d0ae782414
박건규 <geongyu09@gmail.com>
2025-09-29 16:44:00 +0900

    chore(api): api 로직을 모아둔 api 워크스페이스 환경 설정

---
+++ packages/api/tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  format: ["cjs", "esm"],
+  dts: {
+    compilerOptions: {
+      incremental: false,
+      composite: false,
+    },
+  },
+  external: ["axios", "@tanstack/react-query"],
+});
