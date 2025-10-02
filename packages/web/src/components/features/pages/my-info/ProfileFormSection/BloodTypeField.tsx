395db7223df8b79a337940efb3af20c4d50b38e3
박건규 <geongyu09@gmail.com>
2025-10-02 10:15:43 +0900

    feat(web): BloodTypeField 구현
    #20

--- packages/web/src/components/features/pages/my-info/ProfileFormSection/BloodTypeField.tsx
+++ packages/web/src/components/features/pages/my-info/ProfileFormSection/BloodTypeField.tsx
@@ -1,20 +1,20 @@
 import { useMyProfileFormContext } from "@hooks/feature/form/useMyProfileForm";
-import TextField from "@shared/ui/TextField";
+import Spacing from "@shared/layout/Spacing";
+import BloodTypeWheelPickerInput from "@widgets/etc/BloodTypeWheelPickerInput";
 
 const BloodTypeField = () => {
   const { setValue, watch } = useMyProfileFormContext();
 
   return (
-    <TextField
-      label="혈액형"
-      placeholder="B"
-      color="white"
-      value={watch("bloodType")}
-      onChange={(e) => {
-        //TODO: select box로 변경
-        setValue("bloodType", e.target.value);
-      }}
-    />
+    <>
+      <span className="font-bold">혈액형</span>
+      <Spacing size={2} />
+      <BloodTypeWheelPickerInput
+        value={watch("bloodType")}
+        onChange={(value) => setValue("bloodType", value)}
+        placeholder="혈액형을 선택하세요"
+      />
+    </>
   );
 };
 
