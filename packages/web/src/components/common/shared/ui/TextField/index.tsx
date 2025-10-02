e16dd5548f5528829d456d626491fa87a072ef50
박건규 <geongyu09@gmail.com>
2025-10-03 00:26:39 +0900

    feat(web): TextField에서 padding값과 left 값을 받을 수 있도록 추가
    #20

--- packages/web/src/components/common/shared/ui/TextField/index.tsx
+++ packages/web/src/components/common/shared/ui/TextField/index.tsx
@@ -5,27 +5,42 @@ import { ComponentProps, JSX } from "react";
 interface TextFieldProps extends ComponentProps<"input"> {
   label: string;
   subtitle?: string;
+  titleSideComponent?: JSX.Element;
   right?: JSX.Element;
+  left?: JSX.Element;
+  paddingInline?: number;
+  paddingLeft?: number;
+  paddingRight?: number;
   helperText?: string;
 }
 
 export default function TextField({
   label,
+  titleSideComponent,
   subtitle,
+  left,
   right,
   helperText,
+  paddingLeft,
+  paddingRight,
+  paddingInline,
   ...inputProps
 }: TextFieldProps) {
   return (
     <>
       <div className="flex items-end gap-2">
         <span className="font-bold">{label}</span>
+        {titleSideComponent && titleSideComponent}
         {subtitle && (
           <span className="text-gray-900 text-xs font-normal">{subtitle}</span>
         )}
       </div>
       <Spacing size={2} />
+
       <div className="relative">
+        {left && (
+          <div className="absolute left-2 top-1/2 -translate-y-1/2">{left}</div>
+        )}
         <input
           className={cn(
             "rounded-lg bg-white border border-gray-300 w-full p-3",
@@ -33,6 +48,15 @@ export default function TextField({
               "text-gray-800": inputProps.disabled,
             },
           )}
+          style={{
+            paddingLeft: paddingLeft ? paddingLeft * 0.25 + "rem" : undefined,
+            paddingRight: paddingRight
+              ? paddingRight * 0.25 + "rem"
+              : undefined,
+            paddingInline: paddingInline
+              ? paddingInline * 0.25 + "rem"
+              : undefined,
+          }}
           {...inputProps}
         />
 
