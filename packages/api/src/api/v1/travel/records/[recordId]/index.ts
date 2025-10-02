0afd6272d08fc3d094952f3843094e937303685a
박건규 <geongyu09@gmail.com>
2025-10-02 15:26:52 +0900

    fix(api): 중복되는 api 함수 명 수정
    #20

--- packages/api/src/api/v1/travel/records/[recordId]/index.ts
+++ packages/api/src/api/v1/travel/records/[recordId]/index.ts
@@ -1,18 +1,18 @@
 import { AxiosInstance } from "axios";
 
-export const TRAVEL_RECORDS_DELETE_API_PATH = (recordId: string) =>
+export const TRAVEL_RECORD_API_PATH = (recordId: string) =>
   `/api/v1/travel/records/${recordId}`;
 
 // TODO: api 변경 반영 필요
 
-// [delete] 알림 조회
-export const deleteAlert = async (
+// [delete] 산행 기록 삭제
+export const deleteTravelRecord = async (
   instance: AxiosInstance,
   recordId: string,
 ) => {
   const response = await instance({
     method: "DELETE",
-    url: TRAVEL_RECORDS_DELETE_API_PATH(recordId),
+    url: TRAVEL_RECORD_API_PATH(recordId),
   });
 
   return response.data;
