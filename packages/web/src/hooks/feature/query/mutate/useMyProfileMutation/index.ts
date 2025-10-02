58e8f85b862f41aad7171aac7b92d484b082e7fa
박건규 <geongyu09@gmail.com>
2025-10-03 00:25:43 +0900

    chore(web): qpi 명세서 이름을 보고 쿼리를 찾을 수 있도록 query hook에 주석 추가
    #20

--- packages/web/src/hooks/feature/query/mutate/useMyProfileMutation/index.ts
+++ packages/web/src/hooks/feature/query/mutate/useMyProfileMutation/index.ts
@@ -1,12 +1,19 @@
 import authenticatedApi from "@/api/_instances/authenticatedApi";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { putProfile, PutProfileRequest, USER_PROFILE_API_PATH } from "api";
 
+// 프로필 수정 (전체)
 const useMyProfileMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [USER_PROFILE_API_PATH],
     mutationFn: (profileData: PutProfileRequest) =>
       putProfile(authenticatedApi, profileData),
+
+    onSuccess: () => {
+      // TODO: 낙관적 업데이트로 변경하기
+      queryClient.invalidateQueries({ queryKey: [USER_PROFILE_API_PATH] });
+    },
   });
 };
 
--- packages/web/src/hooks/feature/query/query/useProfileQuery/index.ts
+++ packages/web/src/hooks/feature/query/query/useProfileQuery/index.ts
@@ -2,6 +2,7 @@ import authenticatedApi from "@/api/_instances/authenticatedApi";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getProfile, USER_PROFILE_API_PATH } from "api";
 
+// 프로필 조회 (전체)
 const useProfileQuery = () => {
   return useSuspenseQuery({
     queryKey: [USER_PROFILE_API_PATH],
