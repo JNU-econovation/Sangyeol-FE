e60f90e664372ec5f090a5492e094a201be61ec1
박건규 <geongyu09@gmail.com>
2025-10-02 15:33:04 +0900

    feat(web): HeightField에서 타입 안정적으로 onChange 할 수 있도록 로직 추가
    #20

--- packages/web/src/components/features/pages/my-info/ProfileFormSection/HeightField.tsx
+++ packages/web/src/components/features/pages/my-info/ProfileFormSection/HeightField.tsx
@@ -9,10 +9,23 @@ const HeightField = () => {
       label="키"
       placeholder="100"
       color="white"
-      value={watch("height")}
+      value={watch("height")?.toString() ?? ""}
       type="tel"
       maxLength={3}
-      onChange={(e) => setValue("height", +e.target.value)} //TODO: 타입 안정성 챙기기 string -> number
+      onChange={(e) => {
+        const nextValue = e.target.value;
+
+        if (nextValue === "") {
+          setValue("height", undefined, { shouldDirty: true });
+          return;
+        }
+
+        const parsedValue = Number(nextValue);
+
+        if (!Number.isNaN(parsedValue)) {
+          setValue("height", parsedValue, { shouldDirty: true });
+        }
+      }}
       right={<span className="text-gray-900 text-lg">cm</span>}
     />
   );
