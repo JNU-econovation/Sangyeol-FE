aa74feb7aa3114ffc83bc5a4d9bd486163f29fa4
박건규 <geongyu09@gmail.com>
2025-10-03 20:49:21 +0900

    feat(web): TravelLogCalendarSection 구현
    #20

--- packages/web/src/components/features/pages/my-info/TravelLogCalendarSection/index.tsx
+++ packages/web/src/components/features/pages/my-info/TravelLogCalendarSection/index.tsx
@@ -1,10 +1,54 @@
 "use client";
 
-import Calendar from "@entities/Calendar";
+import ROUTE from "@/constants/route";
+import CalendarLayout from "@entities/CalendarLayout";
+import useTravelRecordListQuery from "@hooks/feature/query/query/useTravelRecordListQuery";
+import PolygonIcon from "@icons/PolygonIcon";
 import WeekPolygonIcon from "@icons/WeekPolygonIcon";
-// import PolygonIcon from "@icons/PolygonIcon";
+import Spacing from "@shared/layout/Spacing";
+import { Suspense } from "@suspensive/react";
+import { useRouter } from "next/navigation";
 
-export default function TravelLogCalendarSection() {
-  //TODO: content는 서버에서 받은 값에서 등산기록이 있는 날인지 확인 후 switchcase로 하면 됨
-  return <Calendar content={() => <WeekPolygonIcon />} />;
-}
+const TravelLogCalendarSection = Suspense.with(
+  {
+    fallback: <CalendarLayout content={() => <WeekPolygonIcon />} />,
+  },
+  () => {
+    const {
+      data: { records },
+    } = useTravelRecordListQuery({ year: 2024, month: 6 });
+
+    const router = useRouter();
+
+    return (
+      <section className="bg-white">
+        <CalendarLayout
+          onDateClick={(date) => {
+            router.replace(
+              ROUTE.TRAVEL_LOG({
+                year: date.getFullYear(),
+                month: date.getMonth() + 1,
+                date: date.getDate(),
+              }),
+            );
+          }}
+          content={({ date }) => {
+            const recordForTheDay = records.find((record) => {
+              const recordDate = new Date(record.date);
+              return (
+                recordDate.getFullYear() === date.getFullYear() &&
+                recordDate.getMonth() === date.getMonth() &&
+                recordDate.getDate() === date.getDate()
+              );
+            });
+            if (recordForTheDay) return <PolygonIcon />;
+            return <WeekPolygonIcon />;
+          }}
+        />
+        <Spacing size={4} />
+      </section>
+    );
+  },
+);
+
+export default TravelLogCalendarSection;
