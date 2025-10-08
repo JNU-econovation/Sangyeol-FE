977695e5fb0d6e154ce86ad1174312175b0d1e83
박건규 <geongyu09@gmail.com>
2025-10-09 01:13:56 +0900

    fix(app): 요청에 토큰 postfix로 Bearer를 붙이도록 수정

--- packages/web/src/app/my-page/change-phone-number/page.tsx
+++ packages/web/src/app/my-page/change-phone-number/page.tsx
@@ -1,6 +1,16 @@
-import ChangeNumberFormSection from "@pages/change-phone-number/ChangeNumberFormSection";
+//TODO: 페이지 컴포넌트가 클라이언트 컴포넌트임
+
+"use client";
+
+// import ChangeNumberFormSection from "@pages/change-phone-number/ChangeNumberFormSection";
 import ChangePhoneNumberHeaderSection from "@pages/change-phone-number/ChangePhoneNumberHeaderSection";
 import Spacing from "@shared/layout/Spacing";
+
+import dynamic from "next/dynamic";
+const ChangeNumberFormSection = dynamic(
+  () => import("@pages/change-phone-number/ChangeNumberFormSection"),
+  { ssr: false },
+);
 
 export default function ChangePhoneNumberPage() {
   return (
