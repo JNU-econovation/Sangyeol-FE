76b470653729b5261e82462c6b8a54136ca94341
박건규 <geongyu09@gmail.com>
2025-10-08 22:29:00 +0900

    feat(web): 웹 모달을 프로바이더 내에 띄워지도록 수정
    #20

--- packages/web/src/service/modal/components/CreatePortal/index.tsx
+++ packages/web/src/service/modal/components/CreatePortal/index.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { createPortal } from "react-dom";
-
 import getQueue from "../../core";
 
 export default function CreatePortal() {
   const Queue = getQueue();
-
-  if (typeof document === "undefined") return null;
-
-  const modalRoot = document?.getElementById("modal-root");
-  if (!modalRoot) return null;
 
   if (!Queue.queueItems.length) return null;
 
@@ -18,5 +11,5 @@ export default function CreatePortal() {
 
   if (!currentModal) return null;
 
-  return createPortal(<>{currentModal.modalComponent}</>, modalRoot);
+  return <>{currentModal.modalComponent}</>;
 }
