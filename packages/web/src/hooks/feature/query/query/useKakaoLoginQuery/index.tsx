57f3646984053e6942c68db94ed8a012f4f1b5d6
박건규 <geongyu09@gmail.com>
2025-10-14 00:51:49 +0900

    feat: 개발 모드에서는 카카오 로그인으로 바로 로그인이 가능하도록 구현
    #29

--- packages/web/src/hooks/feature/query/query/useKakaoLoginQuery/index.tsx
+++ packages/web/src/hooks/feature/query/query/useKakaoLoginQuery/index.tsx
@@ -3,6 +3,10 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 import publicApi from "@/api/_instances/publicApi";
 
 const useKakaoLoginQuery = () => {
+  if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
+    window.location.href =
+      "/social-login-loading?accessToken=dev-token&refreshToken=dev-token&accessTokenExpiredTime=9999999999";
+  }
   return useSuspenseQuery({
     queryKey: [KAKAO_LOGIN_URI],
     queryFn: () => getKakaoLoginApi(publicApi),
