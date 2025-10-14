197eb77b3ceade82ee08b20388aa0b90b396fadc
박건규 <geongyu09@gmail.com>
2025-10-14 18:07:39 +0900

    fix(app): useKakaoLoginQuery 훅 본문에서 사이드 이펙트를 직접 실행하는 부분을 useEffect로 수정
    #29

--- packages/web/src/hooks/feature/query/query/useKakaoLoginQuery/index.tsx
+++ packages/web/src/hooks/feature/query/query/useKakaoLoginQuery/index.tsx
@@ -1,12 +1,16 @@
+import { useEffect } from "react";
 import { getKakaoLoginApi, KAKAO_LOGIN_URI } from "api";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import publicApi from "@/api/_instances/publicApi";
 
 const useKakaoLoginQuery = () => {
-  if (process.env.NEXT_PUBLIC_NODE_ENV === "development") {
-    window.location.href =
-      "/social-login-loading?accessToken=dev-token&refreshToken=dev-token&accessTokenExpiredTime=9999999999";
-  }
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "development" && typeof window !== "undefined") {
+      window.location.href =
+        "/social-login-loading?accessToken=dev-token&refreshToken=dev-token&accessTokenExpiredTime=9999999999";
+    }
+  }, []);
+
   return useSuspenseQuery({
     queryKey: [KAKAO_LOGIN_URI],
     queryFn: () => getKakaoLoginApi(publicApi),
