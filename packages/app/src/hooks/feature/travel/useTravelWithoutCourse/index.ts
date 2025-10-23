31a1cb5ea304bdb8039b6f106e2a395406bbbf53
박건규 <geongyu09@gmail.com>
2025-10-23 12:16:18 +0900

    feat(app): 경로를 선택하지 않은 산행 훅 구현
    - 메시지 타입 정리
    - 메시지와 관련한 상수 데이터 추가
    - 산행 로직 구현
    
    #27

--- packages/app/src/hooks/feature/travel/useTravelWithoutCourse/index.ts
+++ packages/app/src/hooks/feature/travel/useTravelWithoutCourse/index.ts
@@ -1,110 +1,148 @@
-// import useSetMapPolylineBridge from "@hooks/feature/bridge/useSetMapPolylineBridge";
 import useRealTimeLocation from "@hooks/feature/location/useRealTimeLocation";
+import useDeviationToast from "@hooks/feature/toast/travel/useDeviationToast";
+import useTravelEndToast from "@hooks/feature/toast/travel/useTravelEndToast";
 import SocketManager from "@service/socket/manager";
-import useToast from "@service/toast";
 import { useTokenStore } from "@store/secureStorage/useTokenStore";
 import useTravelStateStore from "@store/travel";
 import * as Location from "expo-location";
 import { router } from "expo-router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
+
+import { AppState } from "react-native";
+import SOCKET from "./constants";
+import { SocketMessageResponse } from "./types";
 
 const TRAVEL_SOCKET_URL = process.env.EXPO_PUBLIC_TRAVEL_SOCKET_URL;
 const TRAVEL_LOCATION_UPDATE_INTERVAL = 2000;
-const TRAVEL_SOCKET_INTERVAL = 12000;
-
-// TODO: 최적화 필요. | 잠깐 백그라운드에 있다가 오면 몇 초 이상 멈춰있음. 추가적으로 백그라운드에서 상태를 관리하는 로직이 올바르게 동작하지 않을 수 있음. sqlite 도입 고려 필요
+const KEEP_ALIVE_INTERVAL = 5000;
 
 const useTravelWithoutCourse = () => {
+  "use memo";
+  const appState = useRef(AppState.currentState);
   const socketManager = SocketManager.getInstance();
   const { accessToken } = useTokenStore.getState();
+  const intervalRef = useRef<number>(null);
   const {
+    travelState,
     setTravelState,
     setDistance,
     intervalId,
     addTimelog, // (type: 'start' | 'pause' | 'restart' | 'end', time: number) => void. 여행 타임로그 추가 함수. 이는 올바른 시간 계산을 위하여 로그를 스택으로 남기는 함수이다.
-    setIntervalId,
     clearIntervalId,
     pushTraveledPath,
+    getElapsedTime,
     setTravelType,
     setConnectedURL,
     reset,
   } = useTravelStateStore();
-  const showToast = useToast();
-  // const { ref, sendSetMapPolylineMessage } = useSetMapPolylineBridge();
   const { location } = useRealTimeLocation({
     accuracy: "highest",
     timeInterval: TRAVEL_LOCATION_UPDATE_INTERVAL,
     distanceInterval: 1,
   });
-  const [shouldStartTravel, setShouldStartTravel] = useState(false);
+  const { showDeviationToast } = useDeviationToast();
+  const { showTravelEndToast } = useTravelEndToast();
 
+  // init
   useEffect(() => {
-    (async () => {
-      if (!TRAVEL_SOCKET_URL) {
-        console.warn("[useTravelWithCourse] 소켓 URL이 정의되지 않았습니다.");
-        return;
-      }
-      let socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
-      if (socket) {
-        let { latitude, longitude } = location.coords;
-        pushTraveledPath([longitude, latitude]);
-        console.log("[useTravelWithoutCourse] 위치 변경:");
-        socket.sendMessage({
-          event: "current-position",
-          data: {
-            coordinate: [longitude, latitude],
-          },
-        });
-      }
-    })();
-  }, [location, socketManager, pushTraveledPath]);
+    setTravelType("without-course");
+  }, [setTravelType]);
 
-  // location이 준비되면 start 메시지 전송
-  useEffect(() => {
-    (async () => {
-      if (!TRAVEL_SOCKET_URL) {
-        console.warn(
-          "[useTravelWithoutCourse] 소켓 URL이 정의되지 않았습니다.",
-        );
-        return;
-      }
-      const socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
-      // 현재 위지를 단발성으로 가져와서
-      let { latitude, longitude } = location.coords;
-      // traveledPath에 추가 (traveledPath는 사용자의 위치를 배열로 저장하고, 이를 화면에 그린다.)
-      pushTraveledPath([longitude, latitude]);
-      // 서버로 start 메시지 전송
-      if (socket && shouldStartTravel) {
-        socket.sendMessage({
-          event: "start",
-          data: {
-            coordinate: [longitude, latitude],
-            time: Date.now(),
-          },
-        });
-        setShouldStartTravel(false);
-      }
-    })();
-  }, [shouldStartTravel]);
+  const onMessage = async ({ event, status, data }: SocketMessageResponse) => {
+    console.log("소캣 메시지 수신:", { event, status, data });
 
-  // 소켓 연결 및 메시지 처리 함수
+    if (status === "error") return console.error("에러 발생");
+
+    if (event === "start") {
+      // 산행 시작 상태로 바꾸기
+      setTravelState("in-progress");
+      addTimelog("start", Date.now());
+    }
+
+    if (event === "current-position") {
+      const { isArrived, isDeviation, travelDistance } = data;
+      setDistance(travelDistance);
+
+      // 경로 이탈한 경우
+      if (isDeviation) showDeviationToast();
+
+      // 도착한 경우
+      if (isArrived) {
+        // 설정 상태 초기화
+        setTravelState("completed");
+        addTimelog("end", Date.now());
+        // clearIntervalId();
+
+        showTravelEndToast();
+
+        const {
+          coords: { longitude, latitude },
+        } = location;
+
+        socketManager
+          .getSocket(TRAVEL_SOCKET_URL)
+          ?.sendMessage(
+            SOCKET.MESSAGE.END([longitude, latitude], getElapsedTime()),
+          );
+
+        // 1.5초 뒤에 소켓 연결 종료
+        // 3초 뒤에 이전 화면으로 이동
+        setTimeout(() => {
+          socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
+          setTravelState("completed");
+        }, 1500);
+
+        setTimeout(() => {
+          router.replace(`/travel/withoutCourse/result`);
+        }, 3000);
+      }
+    }
+
+    if (event === "pause") {
+      console.log("[useTravelCourse] 여행 일시 정지:", data);
+      setTravelState("paused");
+      addTimelog("pause", Date.now());
+    }
+
+    if (event === "restart") {
+      console.log("[useTravelCourse] 여행 재시작:", data);
+      setTravelState("in-progress");
+      addTimelog("restart", Date.now());
+    }
+
+    if (event === "end") {
+      console.log("[useTravelCourse] 여행 끝:", data);
+      if (intervalId) clearIntervalId();
+      setTravelState("completed");
+
+      addTimelog("end", Date.now());
+
+      // 서버 응답 후 안전하게 소켓 연결 해제
+      setTimeout(() => {
+        socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
+        setTravelState("completed");
+      }, 500);
+
+      router.replace("/travel/withoutCourse/result");
+    }
+  };
+
   const connect = () => {
-    if (!TRAVEL_SOCKET_URL) {
-      console.warn("[useTravelCourse] 소켓 URL이 정의되지 않았습니다.");
-      return;
-    }
-    if (!accessToken) {
-      console.warn("[useTravelCourse] 토큰이 정의되지 않았습니다.");
-      return;
-    }
+    if (!TRAVEL_SOCKET_URL)
+      return console.warn(
+        "[useTravelCourse_start] 소켓 URL이 정의되지 않았습니다.",
+      );
+    if (!accessToken)
+      return console.warn(
+        "[useTravelCourse_start] 토큰이 정의되지 않았습니다.",
+      );
 
-    // 새로운 소켓 연결 생성
     socketManager.makeNewConnection({
       url: TRAVEL_SOCKET_URL,
       token: accessToken,
       onOpen: () => {
         console.log("[useTravelCourse] 소켓 연결 성공");
-        setShouldStartTravel(true);
+        // setShouldStartTravel(true);
         setConnectedURL(TRAVEL_SOCKET_URL);
       },
       onClose: () => {
@@ -118,142 +156,111 @@ const useTravelWithoutCourse = () => {
         setConnectedURL(null);
         reset();
       },
-      onMessage: ({ event, status, data }) => {
-        // console.log("소캣 메시지 수신:", { event, status, data });
-        if (event === "current-position" && status === "success" && data) {
-          // console.log("[useTravelCourse] 현재 위치:", data);
-
-          if (
-            typeof data.index === "number" &&
-            typeof data.isArrived === "boolean" &&
-            typeof data.isDeviation === "boolean"
-          ) {
-            const { isArrived, isDeviation, travelDistance } = data;
-            setDistance(travelDistance);
-
-            // 만약 도착 완료 상태라면
-            if (isArrived) {
-              // 설정값들 초기화(종료 시 상태로 변경)
-              setTravelState("completed");
-              addTimelog("end", Date.now());
-              showToast({
-                type: "success",
-                text1: "여행이 완료되었습니다.",
-                text2: "즐거운 여행 되세요!",
-              });
-
-              // 반복해서 웹소켓으로 위치 전송하는 인터벌 제거
-              clearIntervalId();
-
-              // 종료
-              Location.getCurrentPositionAsync({}) // 현재 위치를 다시 한 번 받아서
-                .then(({ coords: { longitude, latitude } }) => {
-                  // 서버로 end 메시지 전송
-                  socketManager.getSocket(TRAVEL_SOCKET_URL)?.sendMessage({
-                    event: "end",
-                    data: {
-                      coordinate: [longitude, latitude],
-                      time: Date.now(),
-                    },
-                  });
-                })
-                .then(() => {
-                  // 1.5초 뒤에 소켓 연결 종료
-                  // 3초 뒤에 이전 화면으로 이동
-                  setTimeout(() => {
-                    socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
-                    setTravelState("completed");
-                  }, 1500);
-
-                  setTimeout(() => {
-                    router.replace("/travel/withoutCourse/result");
-                  }, 3000);
-                });
-            }
-
-            if (isDeviation) {
-              showToast({
-                type: "error",
-                text1: "경로 이탈 감지",
-                text2: "정해진 경로를 벗어났습니다.",
-              });
-            }
-          }
-        }
-        // 여행 시작 요청에 대한 응답 처리
-        if (event === "start" && status === "success" && data) {
-          // console.log("[useTravelCourse] 여행 시작:", data);
-          setTravelState("in-progress");
-          addTimelog("start", Date.now());
-          if (intervalId) {
-            clearInterval(intervalId);
-            setIntervalId(null);
-          }
-          const newIntervalId = setInterval(async () => {
-            if (!TRAVEL_SOCKET_URL) {
-              console.warn(
-                "[useTravelWithCourse] 소켓 URL이 정의되지 않았습니다.",
-              );
-              return;
-            }
-            let socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
-            //TODO: 현재는 주기적으로 보내도록 하지만, 추후에는 위치 정보를 n초 이상 보내지 않을 경우에 보내도록 최적화 필요
-            if (socket) {
-              let { latitude, longitude } = location.coords;
-              pushTraveledPath([longitude, latitude]);
-              socket.sendMessage({
-                event: "current-position",
-                data: {
-                  coordinate: [longitude, latitude],
-                },
-              });
-            }
-          }, TRAVEL_SOCKET_INTERVAL);
-          setIntervalId(newIntervalId);
-          setTravelType("without-course");
-        }
-        if (event === "pause" && status === "success" && data) {
-          console.log("[useTravelCourse] 여행 일시 정지:", data);
-          setTravelState("paused");
-          addTimelog("pause", Date.now());
-        }
-        if (event === "restart" && status === "success" && data) {
-          console.log("[useTravelCourse] 여행 재시작:", data);
-          setTravelState("in-progress");
-          addTimelog("restart", Date.now());
-        }
-        if (event === "end" && status === "success" && data) {
-          console.log("[useTravelCourse] 여행 끝:", data);
-          if (intervalId) clearIntervalId();
-          setTravelState("completed");
-
-          addTimelog("end", Date.now());
-
-          // 서버 응답 후 안전하게 소켓 연결 해제
-          setTimeout(() => {
-            socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
-            setTravelState("completed");
-          }, 500);
-
-          router.replace("/travel/withoutCourse/result");
-        }
-      },
+      onMessage,
     });
   };
 
-  const disconnect = () => {
-    if (!TRAVEL_SOCKET_URL) {
-      console.warn("[useTravelCourse] 소켓 URL이 정의되지 않았습니다.");
-      return;
+  const start = async () => {
+    // [!info] location이 준비되지 않은 상태일 수 있어서 함수 내부에서 다시 가져옴
+
+    const socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
+    if (!socket)
+      return console.warn(
+        "[useTravelCourse_start] 소켓이 연결되지 않았습니다.",
+      );
+
+    if (travelState === "in-progress")
+      return console.warn(
+        "[useTravelCourse_start] 여행이 이미 시작되었습니다.",
+      );
+
+    try {
+      // 위치 권한 요청
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("[useTravelCourse_start] 위치 권한이 거부되었습니다.");
+        return;
+      }
+
+      // 현재 위치 가져오기
+      const {
+        coords: { longitude, latitude },
+      } = await Location.getCurrentPositionAsync();
+      socket.sendMessage(SOCKET.MESSAGE.START([longitude, latitude]));
+      console.log("start 메시지 보냄");
+    } catch (error) {
+      console.error("[useTravelCourse_start] 위치 가져오기 실패:", error);
     }
-    // reset();
   };
 
-  return {
-    // ref,
-    connect,
-    disconnect,
+  // 주기적으로 위치 전송햐는 함수
+  const startLocationInterval = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    if (travelState === "idle" || travelState === "completed") {
+      return;
+    }
+
+    intervalRef.current = setInterval(() => {
+      const socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
+      if (!socket) return;
+      const {
+        coords: { longitude, latitude },
+      } = location;
+      socket.sendMessage(
+        SOCKET.MESSAGE.CURRENT_POSITION([longitude, latitude]),
+      );
+      pushTraveledPath([longitude, latitude]);
+    }, KEEP_ALIVE_INTERVAL);
   };
+
+  //  주기적으로 위치 전송 시작 / 중지 관리 훅
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      appState.current = nextAppState;
+
+      if (nextAppState.match(/inactive|background/)) {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null;
+        }
+        return;
+      }
+
+      if (nextAppState === "active") {
+        startLocationInterval();
+      }
+    });
+
+    if (appState.current === "active") {
+      startLocationInterval();
+    }
+
+    return () => {
+      subscription.remove();
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+    };
+  }, [travelState, location]);
+
+  // 위치가 변경될 때마다 위치 전송
+  useEffect(() => {
+    const socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
+    if (!socket) return;
+    if (!location) return;
+    if (travelState !== "in-progress") return;
+    const {
+      coords: { longitude, latitude },
+    } = location;
+    socket.sendMessage(SOCKET.MESSAGE.CURRENT_POSITION([longitude, latitude]));
+    pushTraveledPath([longitude, latitude]);
+  }, [location, travelState]);
+
+  return { connect, start };
 };
 
 export default useTravelWithoutCourse;
