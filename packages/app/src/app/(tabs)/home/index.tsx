b9d5426e6fd83d246325161f201f06a26047fdf3
박건규 <geongyu09@gmail.com>
2025-10-25 09:38:02 +0900

    chore(app): 빌드 없이 업데이트를 할 수 있도록 환경 설정

--- packages/app/src/app/(tabs)/home/index.tsx
+++ packages/app/src/app/(tabs)/home/index.tsx
@@ -1,3 +1,5 @@
+import * as Updates from "expo-updates";
+import { Button } from "react-native";
 import styled from "@emotion/native";
 import useRouterPrefetcher from "@hooks/common/useRouterPrefetcher";
 import usePersonalInfoModal from "@hooks/feature/modal/usePersonalInfoModal";
@@ -27,8 +29,25 @@ const HomeScreen = () => {
     }
   }, [profileStatusData]);
 
+  const checkForUpdates = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        // 업데이트 다운로드 완료, 앱 재시작
+        await Updates.reloadAsync();
+      } else {
+        alert("최신 버전입니다!");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container source={require("@assets/images/Home_Background.png")}>
+      <Button title="업데이트 확인" onPress={checkForUpdates} />;
       <HomeNavGridSection />
       <Spacing size={20} />
     </Container>
