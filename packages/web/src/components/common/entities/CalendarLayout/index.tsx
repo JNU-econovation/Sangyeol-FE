c13d844e5ebc23972940c8c060eeb4d15f859c7a
박건규 <geongyu09@gmail.com>
2025-11-06 23:20:03 +0900

    feat(web): 산행 기록 달력에 year/day picker 추가
    - ncdai/react-wheel-picker 의존성 추가
    - picker 공통 컴포넌트 구현
    
    #37

--- packages/web/src/components/common/entities/CalendarLayout/index.tsx
+++ packages/web/src/components/common/entities/CalendarLayout/index.tsx
@@ -5,7 +5,8 @@ import LeftChevronThinIcon from "@icons/LeftChevronThinIcon";
 import RightChevronThinIcon from "@icons/RightChevronThinIcon";
 import Spacing from "@shared/layout/Spacing";
 import Text from "@shared/ui/Text";
-import { memo, ReactNode, useState } from "react";
+import { memo, ReactNode, useEffect, useRef, useState } from "react";
+import MonthPicker from "../MonthPicker";
 
 const DAY = ["일", "월", "화", "수", "목", "금", "토"];
 const LAST_DATE = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -47,8 +48,10 @@ const CalendarLayout = memo(function Calendar({
   content,
   onDateClick,
 }: CalendarProps) {
+  const monthPicker = useRef<typeof MonthPicker>(null);
   const [date, setDate] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [openMonthPicker, setOpenMonthPicker] = useState(false);
 
   const currentYear = date.getFullYear();
   const currentMonth = date.getMonth();
@@ -62,10 +65,33 @@ const CalendarLayout = memo(function Calendar({
   const handleNextMonth = () => {
     setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
   };
+
+  const handleMonthPickerOpen = () => {
+    setOpenMonthPicker(true);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        monthPicker.current &&
+        !(monthPicker.current as any).contains(event.target)
+      ) {
+        setOpenMonthPicker(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [monthPicker]);
+
   return (
     <div>
       <div className="w-full flex justify-center">
-        <span className="font-semibold text-lg">{currentYear}년</span>
+        <button onClick={handleMonthPickerOpen}>
+          <span className="font-semibold text-lg">{currentYear}년</span>
+        </button>
       </div>
       <Spacing size={4} />
       <div className="flex items-center border-b border-gray-300 pb-2">
@@ -135,6 +161,17 @@ const CalendarLayout = memo(function Calendar({
         ))}
       </div>
       <Spacing size={4} />
+
+      {openMonthPicker && (
+        <MonthPicker
+          onConfirm={({ year, month }) => {
+            setDate(new Date(year, month, 1));
+            setSelectedDate(new Date(year, month, 1));
+            setOpenMonthPicker(false);
+          }}
+          onCancel={() => setOpenMonthPicker(false)}
+        />
+      )}
     </div>
   );
 });
---
+++ packages/web/src/components/common/entities/WheelPicker/index.tsx
@@ -0,0 +1,48 @@
+import "@ncdai/react-wheel-picker/style.css";
+
+import * as WheelPickerPrimitive from "@ncdai/react-wheel-picker";
+import { cn } from "@/utils/cn";
+
+type WheelPickerValue = WheelPickerPrimitive.WheelPickerValue;
+
+type WheelPickerOption<T extends WheelPickerValue = string> =
+  WheelPickerPrimitive.WheelPickerOption<T>;
+
+type WheelPickerClassNames = WheelPickerPrimitive.WheelPickerClassNames;
+
+function WheelPickerWrapper({
+  className,
+  ...props
+}: React.ComponentProps<typeof WheelPickerPrimitive.WheelPickerWrapper>) {
+  return (
+    <WheelPickerPrimitive.WheelPickerWrapper
+      className={cn(
+        "bg-white",
+        "*:data-rwp:first:*:data-rwp-highlight-wrapper:rounded-s-md",
+        "*:data-rwp:last:*:data-rwp-highlight-wrapper:rounded-e-md",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function WheelPicker<T extends WheelPickerValue = string>({
+  classNames,
+  ...props
+}: WheelPickerPrimitive.WheelPickerProps<T>) {
+  return (
+    <WheelPickerPrimitive.WheelPicker
+      classNames={{
+        optionItem: "bg-white text-2xl flex w-full items-center",
+        highlightWrapper: "bg-gray-300 text-2xl",
+        highlightItem: "font-semibold text-lg",
+        ...classNames,
+      }}
+      {...props}
+    />
+  );
+}
+
+export { WheelPicker, WheelPickerWrapper };
+export type { WheelPickerClassNames, WheelPickerOption };
