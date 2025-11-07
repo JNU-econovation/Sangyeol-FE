1f9983316f8682770113a516d657aa02a3c8bb97
박건규 <geongyu09@gmail.com>
2025-11-07 11:41:51 +0900

    feat(app): 상세 신고 페이지에서 신고하기 버튼 선택시 동의 페이지로 이동하도록 수정
    #37

--- packages/app/src/components/feature/screens/report/DetailReportForm/SubmitButton.tsx
+++ packages/app/src/components/feature/screens/report/DetailReportForm/SubmitButton.tsx
@@ -1,30 +1,52 @@
 import { useDetailReportFormContext } from "@hooks/feature/form/useDetailReportForm";
-import useDetailReportSMS from "@hooks/feature/sms/useDetailReportSMS";
 import DefaultButton from "@shared/ui/buttons/DefaultButton";
+import { useDetailReportSMSStore } from "@store/report/useDetailReportSMSStore";
 import { useReportPositionStore } from "@store/report/useReportPositionStore";
+import { router } from "expo-router";
 
 const SubmitButton = () => {
   const { watch } = useDetailReportFormContext();
   const { reportPosition } = useReportPositionStore();
+  const { setSMSContent } = useDetailReportSMSStore();
 
-  const { goSMS, isLoading, sendStatus } = useDetailReportSMS({
-    content: watch("reportContent"),
-    lat: watch("reportLocation").latitude || 0,
-    lng: watch("reportLocation").longitude || 0,
-    reporterName: watch("reporterName"),
-    reporterPhone: watch("reporterPhone"),
-    attachment: watch("attachments"),
-    enable: watch("reportContent").trim().length > 0 && !!reportPosition,
-  });
+  const handleSubmit = () => {
+    setSMSContent({
+      content: watch("reportContent"),
+      lat: reportPosition?.latitude || 0,
+      lng: reportPosition?.longitude || 0,
+      reporterName: watch("reporterName"),
+      reporterPhone: watch("reporterPhone"),
+      attachment: watch("attachments"),
+      enable: watch("reportContent").trim().length > 0 && !!reportPosition,
+    });
 
-  const handleSubmit = () => goSMS();
+    router.push("/report/reportTerm");
+  };
+
+  const checkIsDisabled = () => {
+    const reportContent = watch("reportContent");
+    const reporterName = watch("reporterName");
+    const reporterPhone = watch("reporterPhone");
+    const reportPosition = useReportPositionStore.getState().reportPosition;
+
+    if (
+      reportContent.trim().length === 0 ||
+      reporterName.trim().length === 0 ||
+      reporterPhone.trim().length === 0 ||
+      !reportPosition
+    ) {
+      return true;
+    }
+
+    return false;
+  };
 
   return (
     <DefaultButton
       title="신고하기"
       fullWidth
       onPress={handleSubmit}
-      disabled={isLoading || sendStatus === "sent"}
+      disabled={checkIsDisabled()}
     />
   );
 };
