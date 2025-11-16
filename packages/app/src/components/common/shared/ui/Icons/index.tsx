dba3e58b9af029be06d027ab92d150db03ef859a
박건규 <geongyu09@gmail.com>
2025-11-17 00:48:45 +0900

    feat(app): 상세 신고에서 첨부 파일 변경된 ui 적용
    - rounded_x 정적 아이콘 추가
    - 파일 추가 버튼 ui 수정
    
    #37

--- packages/app/src/components/common/shared/ui/Icons/index.tsx
+++ packages/app/src/components/common/shared/ui/Icons/index.tsx
@@ -38,3 +38,4 @@ export { default as WeekTriangle } from "@assets/icons/Week_Triangle.svg";
 
 // cancel
 export { default as TrashSVG } from "@assets/icons/Trash.svg";
+export { default as Rounded_X } from "@assets/icons/Rounded_X.svg";
