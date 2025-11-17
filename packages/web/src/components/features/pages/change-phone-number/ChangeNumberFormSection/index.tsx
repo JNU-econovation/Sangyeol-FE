5227150e597bbddeb4ce438f8d791691ac2af97e
박건규 <geongyu09@gmail.com>
2025-11-18 00:44:30 +0900

    chore(web): 빌드 에러 처리를 위하여 ChangeNumberFormSection에 서스팬스 추가
    #37

--- packages/web/src/components/features/pages/change-phone-number/ChangeNumberFormSection/index.tsx
+++ packages/web/src/components/features/pages/change-phone-number/ChangeNumberFormSection/index.tsx
@@ -2,26 +2,35 @@
 
 import { useChangeNumberForm } from "@hooks/feature/form/useChangeNumberForm";
 import Spacing from "@shared/layout/Spacing";
+import { Suspense } from "@suspensive/react";
 import { FormProvider } from "react-hook-form";
 
+import ChangeNumberFormSectionLoader from "./loader";
 import PhoneNumberField from "./PhoneNumberField";
 import SubmitButton from "./SubmitButton";
 import VerificationField from "./VerificationField";
 
-export default function ChangeNumberFormSection() {
-  const formProvider = useChangeNumberForm();
+const ChangeNumberFormSection = Suspense.with(
+  {
+    fallback: <ChangeNumberFormSectionLoader />,
+    clientOnly: true,
+  },
+  () => {
+    const formProvider = useChangeNumberForm();
+    return (
+      <section>
+        <FormProvider {...formProvider}>
+          <form>
+            <PhoneNumberField />
+            <Spacing size={8} />
+            <VerificationField />
+            <Spacing size={20} />
+            <SubmitButton />
+          </form>
+        </FormProvider>
+      </section>
+    );
+  },
+);
 
-  return (
-    <section>
-      <FormProvider {...formProvider}>
-        <form>
-          <PhoneNumberField />
-          <Spacing size={8} />
-          <VerificationField />
-          <Spacing size={20} />
-          <SubmitButton />
-        </form>
-      </FormProvider>
-    </section>
-  );
-}
+export default ChangeNumberFormSection;
