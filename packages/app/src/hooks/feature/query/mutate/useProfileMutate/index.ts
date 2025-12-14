2a759324b679764da3f88c69a30b4be9d5d157d3
박건규 <geongyu09@gmail.com>
2025-12-14 17:01:17 +0900

    fix(app): 프로필 관련 쿼리를 캐싱하지 않도록 수정
    앱과 웹의 통일성을 지키기 위하여 우선적으로 캐싱을 하지 않도록 하였습니다
    
    #51

--- packages/app/src/hooks/feature/query/mutate/useProfileMutate/index.ts
+++ packages/app/src/hooks/feature/query/mutate/useProfileMutate/index.ts
@@ -1,16 +1,12 @@
-import { PostProfileRequest, postProfile, USER_PROFILE_API_PATH } from "api";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import authenticatedApi from "@api/_instances/authenticatedApi";
+import { useMutation } from "@tanstack/react-query";
+import { postProfile, PostProfileRequest, USER_PROFILE_API_PATH } from "api";
 
 const useProfileMutate = () => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [USER_PROFILE_API_PATH],
     mutationFn: (data: PostProfileRequest) =>
       postProfile(authenticatedApi, data),
-    onMutate: () => {
-      queryClient.invalidateQueries({ queryKey: [USER_PROFILE_API_PATH] });
-    },
   });
 };
 
--- packages/app/src/hooks/feature/query/query/useProfileQuery/index.ts
+++ packages/app/src/hooks/feature/query/query/useProfileQuery/index.ts
@@ -2,13 +2,13 @@ import authenticatedApi from "@api/_instances/authenticatedApi";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getProfile, USER_PROFILE_API_PATH } from "api";
 
+// ! 앱의 경우, 웹과의 일관성을 지키기 위하여 캐싱을 하지 않습니다.
+
 const useProfileQuery = () => {
   return useSuspenseQuery({
     queryKey: [USER_PROFILE_API_PATH],
     queryFn: () => getProfile(authenticatedApi),
     select: (data) => data.userProfileDTO,
-    staleTime: Infinity,
-    gcTime: Infinity,
   });
 };
 
