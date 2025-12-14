cb03c3dc76aa83eacb94f78fb8f43b9ede37c511
박건규 <geongyu09@gmail.com>
2025-12-14 16:49:57 +0900

    refactor(api): 불필요한 data 중첩 제거

--- packages/app/src/hooks/feature/query/prefetch/useProfileImageUrlPrefetch/index.ts
+++ packages/app/src/hooks/feature/query/prefetch/useProfileImageUrlPrefetch/index.ts
@@ -1,13 +1,13 @@
 import authenticatedApi from "@api/_instances/authenticatedApi";
 import { usePrefetchQuery } from "@tanstack/react-query";
-import { getProfileImageSave, PROFILE_IMAGE_API_PATH } from "api";
+import { getProfileImageUrl, PROFILE_IMAGE_API_PATH } from "api";
 
 // 프로필 이미지 조회
 const useProfileImageUrlPrefetch = () => {
   return usePrefetchQuery({
     queryKey: [PROFILE_IMAGE_API_PATH],
     queryFn: async () => {
-      const res = await getProfileImageSave(authenticatedApi);
+      const res = await getProfileImageUrl(authenticatedApi);
       return res.profileImageUrlDTO;
     },
   });
--- packages/app/src/hooks/feature/query/query/useProfileImageUrlQuery/index.ts
+++ packages/app/src/hooks/feature/query/query/useProfileImageUrlQuery/index.ts
@@ -1,12 +1,12 @@
 import authenticatedApi from "@api/_instances/authenticatedApi";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { getProfileImageSave, PROFILE_IMAGE_API_PATH } from "api";
+import { getProfileImageUrl, PROFILE_IMAGE_API_PATH } from "api";
 
 // 프로필 이미지 조회
 const useProfileImageUrlQuery = () => {
   return useSuspenseQuery({
     queryKey: [PROFILE_IMAGE_API_PATH],
-    queryFn: () => getProfileImageSave(authenticatedApi),
+    queryFn: () => getProfileImageUrl(authenticatedApi),
     select: (data) => data.profileImageUrlDTO.profileImageUrl,
   });
 };
