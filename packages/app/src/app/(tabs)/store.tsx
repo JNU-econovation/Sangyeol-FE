613134402d69411f792a17f75812c2dea6609a35
박건규 <geongyu09@gmail.com>
2026-01-13 18:25:34 +0900

    feat(app): 스토어 탭에 서비스 준비중 스크린 띄우도록 수정
    #67

--- packages/app/src/app/(tabs)/store.tsx
+++ packages/app/src/app/(tabs)/store.tsx
@@ -1,12 +1,7 @@
-import Text from "@components/common/shared/ui/Text";
-import { View } from "react-native";
+import ComingSoonScreen from "@shared/ui/ComingSoonScreen";
 
 const StoreScreen = () => {
-  return (
-    <View>
-      <Text>store</Text>
-    </View>
-  );
+  return <ComingSoonScreen />;
 };
 
 export default StoreScreen;
