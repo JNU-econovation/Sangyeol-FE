b4cf1756ca3f504454cf8f605ee27552532adb23
박건규 <geongyu09@gmail.com>
2026-01-19 22:41:01 +0900

    feat(app): 이용 약관 주소를 관리하는 상수 선언
    #73

--- packages/app/src/components/feature/screens/terms/TermListSection/index.tsx
+++ packages/app/src/components/feature/screens/terms/TermListSection/index.tsx
@@ -5,6 +5,7 @@ import Text from "@shared/ui/Text";
 import { COLORS } from "@styles/colorPalette";
 import { useState } from "react";
 import TermSubmitButton from "../TermSubmitButton";
+import TERM_URL from "@constants/term";
 
 const TermListSection = () => {
   const [require1, setRequire1] = useState(false);
@@ -45,25 +46,25 @@ const TermListSection = () => {
         <TermListItemCard
           title="[필수] 이용약관 동의"
           checked={require1}
-          termLink="https://azure-fahrenheit-0af.notion.site/2025-12-30-2d9ec90d98fc808392e3fe653e745dfa?source=copy_link"
+          termLink={TERM_URL.NECESSARY}
           onPress={() => setRequire1(!require1)}
         />
         <TermListItemCard
           title="[필수] 개인정보 및 민감정보 수집·이용 동의"
           checked={require2}
-          termLink="https://azure-fahrenheit-0af.notion.site/2025-12-30-2deec90d98fc80ee8c4cdcfc9cfeb23e?source=copy_link"
+          termLink={TERM_URL.PRIVACY}
           onPress={() => setRequire2(!require2)}
         />
         <TermListItemCard
           title="[필수] 위치정보 수집·이용 동의"
           checked={require3}
-          termLink="https://azure-fahrenheit-0af.notion.site/2025-12-30-2deec90d98fc80a9acebe435e32b505a?source=copy_link"
+          termLink={TERM_URL.LOCATION}
           onPress={() => setRequire3(!require3)}
         />
         <TermListItemCard
           title="[선택] 광고성 정보 및 마케팅 활용 동의"
           checked={optional}
-          termLink="https://azure-fahrenheit-0af.notion.site/2deec90d98fc8013be97cec37885a786?source=copy_link"
+          termLink={TERM_URL.MARKETING}
           onPress={() => setOptional(!optional)}
         />
       </Container>
