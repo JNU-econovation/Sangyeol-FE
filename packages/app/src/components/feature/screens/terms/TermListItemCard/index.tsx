ff042a1aca0467ceea5dce417b6870082601fa68
박건규 <geongyu09@gmail.com>
2026-01-19 21:29:45 +0900

    feat(app): ChevronLeft 선택시 링크로 이동하도록 구현
    #71

--- packages/app/src/components/feature/screens/terms/TermListItemCard/index.tsx
+++ packages/app/src/components/feature/screens/terms/TermListItemCard/index.tsx
@@ -1,12 +1,16 @@
 import styled from "@emotion/native";
+import { ChevronLeft } from "@shared/ui/Icons";
 import Text from "@shared/ui/Text";
 import TriangleCheckbox from "@shared/ui/TriangleCheckbox";
+import { router } from "expo-router";
+import { Pressable } from "react-native";
 
 interface TermListItemCardProps {
   title: string;
   description?: string;
   content?: React.ReactNode;
   checked: boolean;
+  termLink?: string;
   onPress?: () => void;
 }
 
@@ -15,22 +19,36 @@ const TermListItemCard = ({
   description,
   content,
   checked,
+  termLink,
   onPress,
 }: TermListItemCardProps) => {
+  const openTermLink = () => {
+    if (!termLink) return;
+    router.push(`/webModal/${encodeURIComponent(termLink)}`);
+  };
+
   return (
     <ListContainer>
       <TriangleCheckbox checked={checked} onPress={onPress} />
-      <ListContentContainer>
-        <Text fontSize={20} fontWeight="normal" color="black">
-          {title}
-        </Text>
-        {description && (
-          <Text fontSize={12} fontWeight="normal" color="gray900">
-            {description}
+      <Pressable
+        onPress={openTermLink}
+        style={{ flex: 1, flexDirection: "row" }}
+      >
+        <ListContentContainer>
+          <Text fontSize={14} fontWeight="normal" color="black">
+            {title}
           </Text>
-        )}
-        {content && content}
-      </ListContentContainer>
+          {description && (
+            <Text fontSize={12} fontWeight="normal" color="gray900">
+              {description}
+            </Text>
+          )}
+          {content && content}
+          {/* </Pressable> */}
+        </ListContentContainer>
+        {termLink && <ChevronLeft />}
+        {/* <Pressable onPress={openTermLink}> */}
+      </Pressable>
     </ListContainer>
   );
 };
