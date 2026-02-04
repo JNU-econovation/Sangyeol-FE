7a144d3365d4460e8b9a77440cd7ff0072763cdf
박건규 <geongyu09@gmail.com>
2026-02-04 18:33:14 +0900

    feat(app): 여행 결과 화면에서 취소 버튼 및 헤더 섹션 제거
    #77

--- packages/app/src/app/travel/withoutCourse/result.tsx
+++ packages/app/src/app/travel/withoutCourse/result.tsx
@@ -1,8 +1,7 @@
 import styled from "@emotion/native";
-import TravelResultCancelButton from "@screens/travel/TravelResultCancelButton";
 import TravelResultMapView from "@screens/travel/TravelResultMapView";
+import TravelResultScreenHeaderSection from "@screens/travel/TravelResultScreenHeaderSection";
 import TravelWithoutCourseResultReviewSection from "@screens/travel/TravelWithoutCourseResultReviewSection";
-import PositionBottom from "@shared/layout/PositionBottom";
 import Spacing from "@shared/layout/Spacing";
 import { router } from "expo-router";
 import { useEffect } from "react";
@@ -14,13 +13,10 @@ const ResultScreen = () => {
 
   return (
     <Container>
-      <HeaderContainer>
-        <Spacing size={82} />
-        <TravelResultCancelButton />
-      </HeaderContainer>
+      <Spacing size={52} />
+      <TravelResultScreenHeaderSection />
       <TravelWithoutCourseResultReviewSection />
       <TravelResultMapView />
-      <PositionBottom paddingInline={28}></PositionBottom>
     </Container>
   );
 };
@@ -28,17 +24,7 @@ const ResultScreen = () => {
 const Container = styled.View`
   flex: 1;
   position: relative;
-`;
-
-const HeaderContainer = styled.View`
-  /* position: absolute; */
-  z-index: 20;
-  padding-inline: 24px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-  justify-content: flex-end;
+  background-color: white;
 `;
 
 export default ResultScreen;
