60fc8289d04f3393f5e5d8a9cc1433b8398e4b3c
박건규 <geongyu09@gmail.com>
2026-03-04 13:32:14 +0900

    feat:웹 에러시 에러 페이지가 아닌 이전 페이지로 이동하도록 구현

--- packages/web/src/app/error.tsx
+++ packages/web/src/app/error.tsx
@@ -1,36 +1,30 @@
 "use client";
 
+import useRouteBackBridge from "@/hooks/feature/bridge/useRouteBackBridge";
 import { useEffect } from "react";
+import { useStackLinkBack } from "stack-link";
 
 export default function GlobalErrorPage({
   error,
-  reset,
+  // reset,
 }: {
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const { goBack, canGoBack } = useStackLinkBack();
+  const goBackBridge = useRouteBackBridge();
+
+  const navigateBack = () => {
+    if (!canGoBack) return goBackBridge();
+    goBack({
+      animation: "none",
+    });
+  };
+
   useEffect(() => {
     console.error(error);
+    navigateBack();
   }, [error]);
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen p-4 bg-white text-gray-800">
-      <h2 className="text-2xl font-bold">앗, 문제가 발생했어요!</h2>
-      <p className="mt-4 text-gray-600">
-        페이지를 다시 로드하거나 나중에 다시 시도해 주세요.
-      </p>
-      <button
-        onClick={() => reset()}
-        className="mt-6 px-4 py-2 bg-primary text-white rounded"
-      >
-        다시 시도하기
-      </button>
-      <p className="mt-2 text-sm text-gray-500">
-        {error.message || "오류가 발생했습니다. 다시 시도해 주세요."}
-      </p>
-      {error.digest && (
-        <p className="mt-2 text-xs text-gray-400">오류 코드: {error.digest}</p>
-      )}
-    </div>
-  );
+  return null;
 }
