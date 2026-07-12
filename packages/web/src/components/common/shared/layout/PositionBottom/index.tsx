2dff6359099cd937aae15b4449ec1cbb27f0fe1b
박건규 <geongyu09@gmail.com>
2026-07-12 18:29:00 +0900

    fix(web): PositionBottom 컴포넌트의 위치 기준을 화면으로 수정
    #94

--- packages/web/src/components/common/shared/layout/PositionBottom/index.tsx
+++ packages/web/src/components/common/shared/layout/PositionBottom/index.tsx
@@ -17,7 +17,7 @@ export default function PositionBottom({
 }: PositionBottomProps) {
   return (
     <div
-      className={cn("absolute left-0 w-full")}
+      className={cn("sticky left-0 w-full")}
       style={{ bottom, zIndex, padding: `${padding * 0.25}rem` }}
     >
       {children}
