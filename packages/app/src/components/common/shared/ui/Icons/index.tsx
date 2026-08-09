cd23204da5081ad09149c59bf70825f6f4389807
박건규 <geongyu09@gmail.com>
2026-08-09 13:57:11 +0900

    chore(app): 필요한 정적 에셋 아이콘 추가
    #100

---
+++ packages/app/src/components/common/shared/ui/Icons/index.tsx
@@ -0,0 +1,37 @@
+// Navigation
+export { default as HouseSVG } from "@assets/icons/house.svg";
+export { default as RouteSVG } from "@assets/icons/route.svg";
+export { default as MenuSVG } from "@assets/icons/menu.svg";
+export { default as EllipsisSVG } from "@assets/icons/ellipsis.svg";
+export { default as MountainSnowSVG } from "@assets/icons/mountain-snow.svg";
+
+// Arrow
+export { default as ArrowRightSVG } from "@assets/icons/arrow-right.svg";
+export { default as ChevronLeftSVG } from "@assets/icons/chevron-left.svg";
+export { default as ChevronRightSVG } from "@assets/icons/chevron-right.svg";
+export { default as ChevronDownSVG } from "@assets/icons/chevron-down.svg";
+
+// Course
+export { default as MapSVG } from "@assets/icons/map.svg";
+export { default as MapPinSVG } from "@assets/icons/map-pin.svg";
+export { default as NavigationSVG } from "@assets/icons/navigation.svg";
+export { default as MountainSVG } from "@assets/icons/mountain.svg";
+export { default as FootprintsSVG } from "@assets/icons/footprints.svg";
+export { default as TimerSVG } from "@assets/icons/timer.svg";
+export { default as Clock3SVG } from "@assets/icons/clock-3.svg";
+export { default as TriangleAlertSVG } from "@assets/icons/triangle-alert.svg";
+
+// Weather
+export { default as SunSVG } from "@assets/icons/sun.svg";
+export { default as SunriseSVG } from "@assets/icons/sunrise.svg";
+export { default as SunsetSVG } from "@assets/icons/sunset.svg";
+
+// etc
+export { default as BellSVG } from "@assets/icons/bell.svg";
+export { default as BookmarkSVG } from "@assets/icons/bookmark.svg";
+export { default as SparklesSVG } from "@assets/icons/sparkles.svg";
+export { default as MailSVG } from "@assets/icons/mail.svg";
+export { default as MessageCircleSVG } from "@assets/icons/message-circle.svg";
+export { default as FileTextSVG } from "@assets/icons/file-text.svg";
+export { default as PencilSVG } from "@assets/icons/pencil.svg";
+export { default as InfoSVG } from "@assets/icons/info.svg";
