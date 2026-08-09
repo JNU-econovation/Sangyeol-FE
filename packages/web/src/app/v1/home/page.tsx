fdd443f675abc0f475fdb1821ddecfd69ccb61fc
박건규 <geongyu09@gmail.com>
2026-08-09 20:57:39 +0900

    feat(web): 프로토타입 홈 화면 구현
    #100

---
+++ packages/web/src/app/v1/home/page.tsx
@@ -0,0 +1,13 @@
+import MudeungsanMetaInfoSection from "@/modules/widgets/mountain/MudeungsanMetaInfoSection";
+import Spacing from "@shared/components/primitives/layout/Spacing";
+
+const HomePage = () => {
+  return (
+    <div className="px-6">
+      <Spacing size={4} />
+      <MudeungsanMetaInfoSection />
+    </div>
+  );
+};
+
+export default HomePage;
