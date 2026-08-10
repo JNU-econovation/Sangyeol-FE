6cd44015f33d11837cbaaabd6ed85b1edbb6caaf
박건규 <geongyu09@gmail.com>
2026-08-10 13:53:53 +0900

    feat(web): 홈 화면에서 백그라운드 색상 추가
    #100

--- packages/web/src/app/v1/home/page.tsx
+++ packages/web/src/app/v1/home/page.tsx
@@ -4,7 +4,7 @@ import Spacing from "@shared/components/primitives/layout/Spacing";
 
 const HomePage = () => {
   return (
-    <SafeArea>
+    <SafeArea className="bg-gray-300 min-h-dvh">
       <div className="px-6">
         <Spacing size={4} />
         <MudeungsanMetaInfoSection />
--- packages/web/src/shared/components/primitives/layout/SafeArea/index.tsx
+++ packages/web/src/shared/components/primitives/layout/SafeArea/index.tsx
@@ -1,10 +1,21 @@
 import Spacing from "@shared/components/primitives/layout/Spacing";
 import { PropsWithChildren } from "react";
 
-const SafeArea = ({ children }: PropsWithChildren) => {
+const DEFAULT_TOP_SPACING = 20;
+
+interface SafeAreaProps extends PropsWithChildren {
+  topSpacing?: number;
+  className?: string;
+}
+
+const SafeArea = ({
+  children,
+  topSpacing = DEFAULT_TOP_SPACING,
+  className,
+}: SafeAreaProps) => {
   return (
-    <div>
-      <Spacing size={18} />
+    <div className={className}>
+      <Spacing size={topSpacing} />
       {children}
     </div>
   );
