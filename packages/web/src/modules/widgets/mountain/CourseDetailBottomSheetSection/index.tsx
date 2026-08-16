1d6f9801458ae5043eeb5284d1a0d31d5e9ca3b5
박건규 <geongyu09@gmail.com>
2026-08-16 21:23:33 +0900

    feat(web): 코스 상세 페이지 지도 구현
    #110

--- packages/web/src/modules/widgets/mountain/CourseDetailBottomSheetSection/index.tsx
+++ packages/web/src/modules/widgets/mountain/CourseDetailBottomSheetSection/index.tsx
@@ -4,8 +4,8 @@ import useGetCourseDetails from "@shared/hooks/domain/course/useGetCourseDetails
 import BottomSheet from "@shared/components/composites/BottomSheet";
 import Carousel from "@shared/components/composites/Carousel";
 import BaseSelector from "@modules/widgets/map/BaseSelector";
-import CourseImageInfoSection from "@modules/widgets/map/CourseImageInfoSection";
-import CourseWeatherClothesInfoSection from "@modules/widgets/map/CourseWeatherClothesInfoSection";
+// import CourseImageInfoSection from "@modules/widgets/map/CourseImageInfoSection";
+// import CourseWeatherClothesInfoSection from "@modules/widgets/map/CourseWeatherClothesInfoSection";
 import Spacing from "@shared/components/primitives/layout/Spacing";
 import CourseMetaDataUi from "@shared/components/primitives/ui/CourseMetaDataUi";
 import { useParams } from "next/navigation";
@@ -37,10 +37,12 @@ export default function CourseDetailBottomSheetSection() {
         <Spacing size={4} />
 
         <Carousel
-          items={[
-            <CourseWeatherClothesInfoSection />,
-            <CourseImageInfoSection />,
-          ]}
+          items={
+            [
+              // <CourseWeatherClothesInfoSection />,
+              // <CourseImageInfoSection />,
+            ]
+          }
         />
         <Spacing size={4} />
       </BottomSheet>
