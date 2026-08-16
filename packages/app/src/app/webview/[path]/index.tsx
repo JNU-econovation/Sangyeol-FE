8aebaedabda6f9ddc3870b93f212a45a65c82556
박건규 <geongyu09@gmail.com>
2026-08-16 21:27:54 +0900

    feat(app): 웹뷰에서의 뒤로가기 제스처가 앱의 뒤로가기 제스처를 호출하지 않도록 방어 로직 추가

--- packages/app/src/app/webview/[path]/index.tsx
+++ packages/app/src/app/webview/[path]/index.tsx
@@ -1,12 +1,26 @@
 import WebViewWithInjected from "@shared/components/composites/WebViewWithInjected";
+import { useWebviewHistoryStore } from "@shared/model/webviewHistoryStore";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 
 const WebviewScreen = () => {
   const { path } = useLocalSearchParams<{ path: string }>();
+  const setCanGoBack = useWebviewHistoryStore((state) => state.setCanGoBack);
 
   const uri = "" + process.env.EXPO_PUBLIC_WEB_BASE_URI + path;
 
-  return <WebViewWithInjected source={{ uri }} />;
+  useEffect(() => {
+    return () => {
+      setCanGoBack(false);
+    };
+  }, [setCanGoBack]);
+
+  return (
+    <WebViewWithInjected
+      source={{ uri }}
+      onNavigate={({ canGoBack }) => setCanGoBack(canGoBack)}
+    />
+  );
 };
 
 export default WebviewScreen;
