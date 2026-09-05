c2971a618619e1e23bb953f250fd9308b0896714
박건규 <geongyu09@gmail.com>
2026-09-05 13:31:58 +0900

    fix: 무등산국립공원 오타 수정

--- packages/web/src/modules/widgets/mountain/CourseDetailBottomSheetSection/index.tsx
+++ packages/web/src/modules/widgets/mountain/CourseDetailBottomSheetSection/index.tsx
@@ -56,7 +56,7 @@ export default function CourseDetailBottomSheetSection() {
           <div>
             <h3 className="text-xl font-semibold">{name}</h3>
             {/* 어떤 정보를 뜻하는지 확인 필요 */}
-            <p className="text-sm text-gray-800">무등산 국립공원</p>
+            <p className="text-sm text-gray-800">무등산국립공원</p>
           </div>
           <Button
             className="flex h-11 items-center justify-center gap-2 rounded-[10px] px-3.5 py-0 text-sm"
